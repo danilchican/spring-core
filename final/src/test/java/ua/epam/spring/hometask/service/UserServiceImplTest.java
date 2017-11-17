3c96dcd7d0b6943753b7c57f43853f93fce3474f
Uladzislau Danilchyk <Uladzislau_Danilchyk@epam.com>
2017-11-17 11:30:34 +0300

    Modified test

--- final/src/test/java/ua/epam/spring/hometask/service/UserServiceImplTest.java
+++ final/src/test/java/ua/epam/spring/hometask/service/UserServiceImplTest.java
@@ -15,13 +15,15 @@ import ua.epam.spring.hometask.domain.User;
 import java.util.Collection;
 import java.util.Optional;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = { "classpath:spring.xml" })
+@ContextConfiguration(locations = {"classpath:spring.xml"})
 public class UserServiceImplTest {
 
     @Mock
@@ -37,11 +39,24 @@ public class UserServiceImplTest {
     }
 
     @Test
-    public void getUserByEmailTest() throws Exception {
+    public void getUserByEmailSuccessScenarioTest() throws Exception {
         User expected = new User();
         when(userDAO.getUserByEmail(anyString())).thenReturn(Optional.of(expected));
 
         User actual = userService.getUserByEmail("danilchican@mail.ru");
         assertEquals(expected, actual);
+    }
+
+    @Test
+    public void getUserByEmailFirstFailureScenarioTest() throws Exception {
+        when(userDAO.getUserByEmail(anyString())).thenReturn(Optional.empty());
+        User actual = userService.getUserByEmail("danilchican@mail.ru");
+
+        assertThat(actual, is(nullValue()));
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void getUserByEmailSecondFailureScenarioTest() throws Exception {
+        userService.getUserByEmail(null);
     }
 }
