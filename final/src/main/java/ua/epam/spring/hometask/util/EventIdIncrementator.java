8d8fa942b43c66f1af798d63e38a55624e052a80
danilchican <danilchican@mail.ru>
2017-12-07 22:50:28 +0300

    Removed unused classes.

--- final/src/main/java/ua/epam/spring/hometask/util/EventIdIncrementator.java
+++
@@ -1,9 +0,0 @@
-package ua.epam.spring.hometask.util;
-
-public class EventIdIncrementator {
-    private static Long id = 0L;
-
-    public static Long next() {
-        return ++id;
-    }
-}
--- final/src/main/java/ua/epam/spring/hometask/util/UserIdIncrementator.java
+++
@@ -1,9 +0,0 @@
-package ua.epam.spring.hometask.util;
-
-public class UserIdIncrementator {
-    private static Long id = 0L;
-
-    public static Long next() {
-        return ++id;
-    }
-}
